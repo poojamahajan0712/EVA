de941d6a069638d42eeaa7e421a82fae7f3000a1
poojamahajan0712 <49225713+poojamahajan0712@users.noreply.github.com>
2020-05-18 12:47:29 +0530

    Add files via upload

---
+++ S6/Old_s6/README.md
@@ -0,0 +1,33 @@
+# S6
+EVA_STep1 - showing accuracy and loss graphs for different comb - without L1 and L2 
+With L1
+with L2
+with L1 and L2
+
+2nd file - Showing how to get misclassified images 
+
+######################################################################
+Assignment 7 -details 
+
+#S7 
+1.Copy_of_S7_try2.ipynb is the main file -training CIFAR 10 dataset.
+
+2.Model1.py - CNN Model file being used .
+model2.py -CNN model (with depthwise separable implementation)
+( https://github.com/poojamahajan0712/EVA_Pooja/blob/master/S7_withdepthwise.ipynb )
+
+3.train_iterations.py- for training the network
+train_iterations1.py - with tqdm
+4.test_score.py- for checking how model performs on test data (calculates accuracy)
+
+5.class_wise_accuracy.py- checking class level accuracy 
+
+Assignment8
+
+Added two new modules - data_load_cifar10 - loading cifar data 
+transform_func - for transformations
+
+
+
+
+
---
+++ S6/Old_s6/train_iterations.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  3 20:15:36 2020
+
+@author: pooja
+"""
+
+
+def train(n_epoch,trainloader,device,optimizer,net,criterion):
+    for epoch in range(n_epoch):  # loop over the dataset multiple times
+        print(epoch)
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+    
+            # zero the parameter gradients
+            optimizer.zero_grad()
+    
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+    
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    
+    print('Finished Training')
